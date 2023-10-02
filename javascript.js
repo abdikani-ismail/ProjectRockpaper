65f9fbdb22e495eed693207ee8944684fa7d1828
abdkani-ismail <ibnuismailhassan@gmail.com>
2023-10-02 10:20:50 -0700

    second

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 // console.log("hello world")
 
  const getComputerChoice=()=>{
- const randomNumber=(Math.floor(Math.random())*3)
+ const randomNumber=(Math.floor(Math.random()*3))
+    
     if(randomNumber===0){
- return 'Rock';
+        return 'Rock';
     }
     else if(randomNumber===1){
         return 'Paper'
@@ -12,4 +13,71 @@
         return 'Scissor';
     }
  }
-console.log(getComputerChoice())
+
+function Round(playerSelection, computerSelection){
+    // if(playerSelection==="rock" && computerSelection==="Paper"){
+    //    return "You lose! Paper beats rock"
+    // }
+    // else if(playerSelection==="rock" && computerSelection==="Scissor"){
+    //     return "You win rock beats Scissor"
+    // }
+    // else if (playerSelection==="rock" && computerSelection==="Rock"){
+    //     return "draw"
+    // }
+
+    
+if(playerSelection.toLowerCase()==="rock"){
+    if(computerSelection==="Rock"){
+        return "draw"
+    }
+    else if(computerSelection==="Scissor"){
+        return "you win! Rocks beats Scissor"
+    }
+    else if(computerSelection==="Paper"){
+        return "You lose! Paper beats rock"
+    }
+}
+ else if(playerSelection.toUpperCase()==="SCISSOR"){
+    if(computerSelection==="Scissor"){
+        return "draw"
+    }
+    else if(computerSelection==="Paper"){
+        return "You win! Scissor beats Paper"
+    }
+    else if(computerSelection==="Rock"){
+        return "You lose! Rock beats Scissor"
+    }
+ }
+ else if(playerSelection.toLowerCase()==="paper"){
+    if(computerSelection==="Rock"){
+        return "You win! Paper beats rock"
+    }
+    else if(computerSelection==="Paper"){
+        return "draw"
+    }
+    else if(computerSelection==="Scissor"){
+        return "You lose! Scissor beats Paper"
+    }
+    else{
+        return "Please Enter rock or Paper or Scissor"
+    }
+ }
+}
+
+
+
+
+//(Round("ROCk", choice));
+
+
+function game(){
+
+  
+ 
+    for(let i=1; i<=5; i++){
+        const choice=getComputerChoice()
+        console.log(Round(prompt("Enter your value"), choice))
+        // console.log(x);
+    }
+}
+game()
