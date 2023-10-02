22342bcb1a7f902df8ff2b460a544ffd29061a4e
abdkani-ismail <ibnuismailhassan@gmail.com>
2023-10-01 23:36:39 -0700

    this scissor project

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// console.log("hello world")
+
+ const getComputerChoice=()=>{
+ const randomNumber=(Math.floor(Math.random())*3)
+    if(randomNumber===0){
+ return 'Rock';
+    }
+    else if(randomNumber===1){
+        return 'Paper'
+    }
+    else if (randomNumber===2){
+        return 'Scissor';
+    }
+ }
+console.log(getComputerChoice())
